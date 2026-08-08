d3a4dd58c0c7e1c901eb659c9c06f35ea467ba9b
Caio-Mizohata <caiomizohata@gmail.com>
2026-08-08 00:15:47 -0300

    feat: Reutilização dos componentes "Navbar", "UiBase" e "Certificados"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { UiBase } from "./components/ui-base/ui-base";
+import { Certificados } from "./pages/certificados/certificados";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, UiBase, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
