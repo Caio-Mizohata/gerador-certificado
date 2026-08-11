4ef962cf9ae08db5046fa7313dbb5e6ca76cec87
Caio-Mizohata <caiomizohata@gmail.com>
2026-08-10 22:35:03 -0300

    refactor: Remove o componente de botão secundário e substitui pelo componente de certificações de item

--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { SecondaryButton } from "../../components/secondary-button/secondary-button";
+import { ItemCertificado } from "../../components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButton],
+  imports: [ItemCertificado],
   templateUrl: './certificados.html',
   styleUrl: './certificados.css',
 })
